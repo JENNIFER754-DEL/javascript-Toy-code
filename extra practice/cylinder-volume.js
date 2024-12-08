ed783cc543f8b104c754ca8af66fd023fdb54c94
JENNIFER974-DEL <jennifernyaguthii23@gmail.com>
2024-12-09 00:04:52 +0300

    Add files from extra practice

---
+++ extra practice/cylinder-volume.js
@@ -0,0 +1,12 @@
+class Cylinder {
+    constructor(radius, height) {
+        this.radius = radius;
+        this.height = height;
+    }
+
+    getVolume() {
+        return (Math.PI * Math.pow(this.radius, 2) * this.height).toFixed(4);
+    }
+}
+const cylinder = new Cylinder(3, 5);
+console.log(`Volume of cylinder: ${cylinder.getVolume()}`);
